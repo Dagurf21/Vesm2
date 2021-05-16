e1a8fa806a217ace254a27a9c9799574d1ff9859
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2021-05-16 23:06:56 +0000

    Add files via upload

---
+++ Humidity-censor.ino
@@ -0,0 +1,24 @@
+#include <dht.h>
+#define outPin 8
+dht DHT;
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  int readData = DHT.read11(outPin);
+
+  float t = DHT.temperature;
+  float h = DHT.humidity;
+
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print("°C | ");
+  Serial.print((t*9.0)/5.0+32.0);
+  Serial.print("Humidity = ");
+  Serial.print(h);
+  Serial.println("");
+  
+  delay(2000);
+}
